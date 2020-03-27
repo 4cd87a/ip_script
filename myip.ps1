fa14181a36afe1bf406cf040b1f4f7439c10f097
4cd87a <52211599+4cd87a@users.noreply.github.com>
2020-03-27 15:32:28 +0100

    v1

---
+++ myip.ps1
@@ -0,0 +1,9 @@
+﻿$cont = (wget ifconfig.co/json | select -First 1 -ExpandProperty 'Content') | ConvertFrom-Json
+#Write-Host $cont
+Write-Host "ip = $($cont.ip)"
+Write-Host "country = $($cont.country)"
+Write-Host "city = $($cont.city)"
+Write-Host "hostname = $($cont.hostname)"
+Write-Host "asn org = $($cont.asn_org)"
+PAUSE
+#Write-Host "user agent = $($cont.user_agent)"
