4135c0812790b758f4a9cf4055db79f053284746
4cd87a <52211599+4cd87a@users.noreply.github.com>
2020-03-27 16:49:04 +0100

    add speedtest

--- myip.ps1
+++ myip.ps1
@@ -5,5 +5,29 @@ Write-Host "country = $($cont.country)"
 Write-Host "city = $($cont.city)"
 Write-Host "hostname = $($cont.hostname)"
 Write-Host "asn org = $($cont.asn_org)"
-PAUSE
+
+Write-Host "Press enter to end or spacebar for speedtest"
+
+
+$pressed = $false
+$speedtest = $false
+do {
+    $key = [Console]::ReadKey(“NoEcho,IncludeKeyDown”)
+    $value = $key.KeyChar
+    
+
+    switch($key.key) {
+        'Enter' { $pressed=$true}
+        'Spacebar' { 
+            $pressed=$true
+            $speedtest = $true
+           }
+    }
+}while ($pressed -ne $true)
+
 #Write-Host "user agent = $($cont.user_agent)"
+
+if ($speedtest -eq $true) {
+    curl https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | select -First 1 -ExpandProperty 'Content'| python -
+    PAUSE
+}
